e3d40ad2bb3408bcd98f013da4216b5b9c1cdf6a
Jisu Lee <jisulee.shsf@gmail.com>
2024-08-30 03:31:33 +0900

    영속성 관리 - 영속성 컨텍스트(비영속 및 영속)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,10 +16,16 @@ public class JpaMain {
         tx.begin();
 
         try {
+            //비영속
             Member member = new Member();
             member.setId(1L);
             member.setName("helloA");
+
+            //영속
+            System.out.println("=== before ===");
             em.persist(member);
+            System.out.println("=== after ===");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
