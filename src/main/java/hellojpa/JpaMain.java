503706ac7a7fc4291d6393b4375bd5dbc077fa0e
Jisu Lee <jisulee.shsf@gmail.com>
2024-09-05 02:14:52 +0900

    프록시와 연관관계 관리 - 영속성 전이(CASCADE)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,8 +5,6 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-import java.util.List;
-
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -18,46 +16,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 1. 즉시 로딩
-//            Team team = new Team();
-//            team.setName("team");
-//            em.persist(team);
-//
-//            Member member = new Member();
-//            member.setName("user");
-//            member.setTeam(team);
-//            em.persist(member);
-//
-//            em.flush();
-//            em.clear();
-//
-//            Member findMember = em.find(Member.class, member.getId());
-//            System.out.println("findMember.getTeam().getClass() = " + findMember.getTeam().getClass()); //class hellojpa.Team
-//            System.out.println("findMember.getTeam().getName() = " + findMember.getTeam().getName()); //team
+            Child child1 = new Child();
+            Child child2 = new Child();
+            Parent parent = new Parent();
 
-            //2. N+1 문제
-            Team team1 = new Team();
-            team1.setName("team1");
-            em.persist(team1);
-
-            Team team2 = new Team();
-            team2.setName("team2");
-            em.persist(team2);
-
-            Member member1 = new Member();
-            member1.setName("member1");
-            member1.setTeam(team1);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setName("member2");
-            member2.setTeam(team2);
-            em.persist(member2);
-
-            em.flush();
-            em.clear();
-
-            List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+            parent.addChild(child1);
+            parent.addChild(child2);
+            em.persist(parent);
 
             tx.commit();
         } catch (Exception e) {
