358b342d16cae8c95ae88230f2d981af7ea8e3a2
Jisu Lee <jisulee.shsf@gmail.com>
2024-09-06 14:31:31 +0900

    값 타입 - 값 타입 컬렉션(저장 및 조회)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,9 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.util.List;
+import java.util.Set;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -16,10 +19,35 @@ public class JpaMain {
         tx.begin();
 
         try {
+            //1. 값 타입 저장
             Member member = new Member();
             member.setName("userA");
-            member.setAddress(new Address("city", "street", "zipcode"));
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+
+            member.getFavoriteFoods().add("foodA");
+            member.getFavoriteFoods().add("foodB");
+
+            member.getAddressHistory().add(new Address("cityA", "street", "20000"));
+            member.getAddressHistory().add(new Address("cityB", "street", "310000"));
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("=====");
+
+            //2. 값 타입 조회
+            Member findMember = em.find(Member.class, member.getId());
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for (String favoriteFood : favoriteFoods) {
+                System.out.println("favoriteFood = " + favoriteFood);
+            }
+
+            List<Address> addressHistory = findMember.getAddressHistory();
+            for (Address address : addressHistory) {
+                System.out.println("address = " + address.getCity());
+            }
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,6 +2,11 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Entity
 public class Member {
 
@@ -12,7 +17,16 @@ public class Member {
     private String name;
 
     @Embedded
-    private Address address;
+    private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return Id;
@@ -30,11 +44,27 @@ public class Member {
         this.name = name;
     }
 
-    public Address getAddress() {
-        return address;
+    public Address getHomeAddress() {
+        return homeAddress;
     }
 
-    public void setAddress(Address address) {
-        this.address = address;
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<Address> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
