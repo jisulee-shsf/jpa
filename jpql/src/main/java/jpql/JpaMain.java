cb6a3daf7bfef9ae249927ee2717f854aef54580
Jisu Lee <jisulee.shsf@gmail.com>
2024-09-25 21:51:53 +0900

    객체지향 쿼리 언어 - 경로 표현식

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -22,40 +22,34 @@ public class JpaMain {
             team.setName("teamA");
             em.persist(team);
 
-            Member member = new Member();
-            member.setUsername(null);
-            member.setAge(10);
-            member.setTeam(team);
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("memberA");
+            member1.setTeam(team);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("memberB");
+            member2.setTeam(team);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
             System.out.println("==========");
 
-            //기본 CASE 식
-//            String query = "select " +
-//                    "case when m.age <= 10 then '학생 요금'" +
-//                    "     when m.age >= 60 then '경로 요금'" +
-//                    "     else '일반 요금'" +
-//                    "end " +
-//                    "from Member m";
-//            List<String> result = em.createQuery(query, String.class)
-//                    .getResultList();
-//
-//            for (String s : result) {
-//                System.out.println("s = " + s);
-//            }
+            //상태 필드(탐색 불가)
+            String query1 = "select m.username from Member m where m.id = :id";
+            String result1 = em.createQuery(query1, String.class)
+                    .setParameter("id", member1.getId())
+                    .getSingleResult();
 
-            //coalesce
-            String query = "select coalesce(m.username, 'default') " +
-                    "from Member m";
-            List<String> result = em.createQuery(query, String.class)
-                    .getResultList();
+            //단일 값 연관 경로 - 묵시적 내부 조인 발생(탐색 가능)
+            String query2 = "select m.team from Member m"; // String query2 = "select t from Member m join m.team t";
+            List<Team> result2 = em.createQuery(query2, Team.class).getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
+            //단일 값 연관 경로 - 묵시적 내부 조인 발생(탐색 불가)
+            String query3 = "select t.members from Team t"; //String query3 = "select m from Team t join t.members m";
+            List<Member> result3 = em.createQuery(query3, Member.class).getResultList();
 
             tx.commit();
         } catch (Exception e) {
