86c962e1dc57d98174b7c5e6f94aea673028b53e
Jisu Lee <jisulee.shsf@gmail.com>
2024-09-26 23:44:12 +0900

    객체지향 쿼리 언어 - 페치 조인(기본)

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -18,38 +18,58 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team team1 = new Team();
+            team1.setName("teamA");
+            em.persist(team1);
+
+            Team team2 = new Team();
+            team2.setName("teamB");
+            em.persist(team2);
 
             Member member1 = new Member();
             member1.setUsername("memberA");
-            member1.setTeam(team);
+            member1.setTeam(team1);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("memberB");
-            member2.setTeam(team);
+            member2.setTeam(team1);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("memberC");
+            member3.setTeam(team2);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
             System.out.println("==========");
 
-            //상태 필드(탐색 불가)
-            String query1 = "select m.username from Member m where m.id = :id";
-            String result1 = em.createQuery(query1, String.class)
-                    .setParameter("id", member1.getId())
-                    .getSingleResult();
+            String query1 = "select m from Member m";
+            List<Member> result1 = em.createQuery(query1, Member.class)
+                    .getResultList();
 
-            //단일 값 연관 경로 - 묵시적 내부 조인 발생(탐색 가능)
-            String query2 = "select m.team from Member m"; // String query2 = "select t from Member m join m.team t";
-            List<Team> result2 = em.createQuery(query2, Team.class).getResultList();
+            //다대일 페치 조인 사용
+            String query2 = "select m from Member m join fetch m.team";
+            List<Member> result2 = em.createQuery(query2, Member.class)
+                    .getResultList();
 
-            //단일 값 연관 경로 - 묵시적 내부 조인 발생(탐색 불가)
-            String query3 = "select t.members from Team t"; //String query3 = "select m from Team t join t.members m";
-            List<Member> result3 = em.createQuery(query3, Member.class).getResultList();
+            for (Member member : result2) {
+                System.out.println("member = " + member.getUsername() + " / " + member.getTeam().getName());
+            }
+
+            //일대다 페치 조인 사용
+            String query3 = "select t from Team t join fetch t.members";
+            List<Team> result3 = em.createQuery(query3, Team.class)
+                    .getResultList();
+
+            for (Team team : result3) {
+                System.out.println("team = " + team.getName() + " / " + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println(" -> member = " + member);
+                }
+            }
 
             tx.commit();
         } catch (Exception e) {
